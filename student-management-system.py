5b34763bff193fca5936b0c41d36724e4613b1ee
Mohammed Sohail <sohailmd4622@gmail.com>
2026-08-16 20:52:58 +0530

    Add files via upload
    version 2

--- student-management-system.py
+++ student-management-system.py
@@ -17,8 +17,18 @@ class student:
             print(f"{self.name} has passed!")
         else:
             print(f"{self.name} has failed")
-
-rohan=student('rohan',23,300)
-rohan.details()
-rohan.cal_percentage()
-rohan.pass_fail()
+students = [
+    student('rohan',23,300),
+    student('mohan',24,400),
+    student('rahul',24,500),
+    student('joy',24,250),
+    student('elsa',24,600)
+]
+for s in students:
+    s.details()
+    print("")
+topper = students[0]
+for s in students:
+    if s.marks>topper.marks:
+        topper=s
+topper.details()
